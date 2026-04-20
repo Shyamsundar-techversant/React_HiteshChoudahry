e459654830b04fcb6523ee7bfdb9815d6743c0d1
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-04-20 17:11:12 +0530

    customReact application - part 2

--- vite-app/src/main.jsx
+++ vite-app/src/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const reactElement = {
+    type : 'a',
+    props : {
+        href : "https://google.com",
+        target : "_blank",
+        children : "Click me to visit google"
+    }
+}
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  )
+}
+
+const AnotherElement = (
+  <a href='https://google.com' target='_blank'>Click me to visit Google</a>
+);
+
+const aReactElement = React.createElement(
+  'a',
+  {href : "http://google.com" , target : "_blank"},
+  'click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  aReactElement
 )
