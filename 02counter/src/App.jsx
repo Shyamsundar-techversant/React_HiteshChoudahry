1825fd950231c96c03acc5a79180923e26c2638c
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-04-21 18:22:40 +0530

    counter app

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let counter = 1;
+  const[counter,setCounter] = useState(0);
   const addValue = () => {
-    counter = counter + 1;
-    console.log(counter);
-    
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
   }
   return (
     <>
       <h1>React Course</h1>
       <h2>Counter Value : {counter}</h2>
       <button onClick={addValue}>Add Value</button>{" "}
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
       <p>Footer : {counter}</p>
     </>
   )
